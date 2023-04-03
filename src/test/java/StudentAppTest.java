d1948ecf7632f9b208de8e3b94999ba2b0cd3ac6
PavelsKonins <onetwolv@gmail.com>
2023-04-03 20:08:33 +0300

    [master]: Project upload

--- src/test/java/StudentAppTest.java
+++ src/test/java/StudentAppTest.java
@@ -1,18 +1,63 @@
+import com.github.javafaker.Faker;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import page_objects.AddStudentPage;
+import page_objects.AllStudentsPage;
+import page_objects.Notifications;
+import utils.ConfigHelper;
+import utils.DriverManager;
+
+import java.time.Duration;
+
+import static constants.AllConstants.GenderConstants.MALE;
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+import static utils.ConfigHelper.getConfig;
 
 public class StudentAppTest {
 
-    @Test
-    public void openStudentApp() {
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--remote-allow-origins=*");
-        WebDriver driver = new ChromeDriver(options);
-        driver.get("http://acodemystudentapp-env.eba-d2vctp4d.eu-north-1.elasticbeanstalk.com/");
+    WebDriver driver = DriverManager.getInstance();
+    WebDriverWait driverWait;
+    Faker dataFaker = new Faker();
+    AllStudentsPage allStudentsPage;
+    AddStudentPage addStudentPage;
+    Notifications notifications;
 
+    @BeforeMethod
+    public void initialize() {
+        driverWait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        driver.get(getConfig().getString("student.app.hostname"));
+        allStudentsPage = new AllStudentsPage();
+        addStudentPage = new AddStudentPage();
+        notifications = new Notifications();
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void tearDown() {
         driver.close();
         driver.quit();
+    }
+
+    @Test(description = "Add student and check successful message")
+    public void openStudentApp() {
+        allStudentsPage.waitAndClickOnAddStudentButton();
+
+        String name = dataFaker.name().firstName();
+        addStudentPage.waitAndSetValueForNameField(name);
+        addStudentPage.waitAndSetValueForEmailField();
+        addStudentPage.waitAndSetGender(MALE);
+        addStudentPage.clickOnSubmitButton();
+
+        assertEquals(notifications.getMessageFromNotification(), "Student successfully added");
+        assertEquals(notifications.getDescriptionFromNotification(), name + " was added to the system");
+
+        notifications.getPopUpCloseButton().click();
+        assertTrue(driverWait.until(ExpectedConditions.invisibilityOf(notifications.getPopUpCloseButton())));
     }
 }
